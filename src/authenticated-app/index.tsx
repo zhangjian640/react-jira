734945ca8e3fd4646a8387841229e754524e7b0a
张建 <475218106@qq.com>
2022-07-31 18:26:39 +0800

    feat: 使用Row布局

--- src/authenticated-app/index.tsx
+++ src/authenticated-app/index.tsx
@@ -2,63 +2,41 @@ import React from "react";
 import styled from "@emotion/styled";
 import { useAuth } from "../context/auth-context";
 import { ProjectListScreen } from "../screens/project-list";
+import { Row } from "components/lib";
 export const AuthenticatedApp = () => {
   const { logout } = useAuth();
   return (
     <Container>
-      <Header>
-        <HeaderLeft></HeaderLeft>
+      <Header between={true}>
+        <HeaderLeft gap={true}>
+          <h3>logo</h3>
+          <h3>项目</h3>
+          <h3>用户</h3>
+        </HeaderLeft>
         <HeaderRight>
           <button onClick={logout}>登出</button>
         </HeaderRight>
       </Header>
-      <Nav>nav</Nav>
       <Main>
         <ProjectListScreen />
       </Main>
-      <Aside>aside</Aside>
-      <Footer>footer</Footer>
     </Container>
   );
 };
 
 const Container = styled.div`
   display: grid;
-  grid-template-rows: 6rem 1fr 6rem;
-  grid-template-columns: 20rem 1fr 20rem;
-  grid-template-areas:
-    "header header header"
-    "nav main aside"
-    "footer footer footer";
-  grid-gap: 10rem;
+  grid-template-rows: 6rem 1fr;
   height: 100vh;
 `;
 
-const Header = styled.header`
-  grid-area: header;
-  display: flex;
-  align-items: center;
+const Header = styled(Row)`
   justify-content: space-between;
 `;
 
-const HeaderLeft = styled.div`
+const HeaderLeft = styled(Row)`
   display: flex;
   align-items: center;
 `;
 const HeaderRight = styled.div``;
-
-const Main = styled.main`
-  grid-area: main;
-`;
-
-const Nav = styled.nav`
-  grid-area: nav;
-`;
-
-const Aside = styled.aside`
-  grid-area: aside;
-`;
-
-const Footer = styled.footer`
-  grid-area: footer;
-`;
+const Main = styled.div``;
